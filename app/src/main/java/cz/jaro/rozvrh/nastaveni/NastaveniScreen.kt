8526955e6f87f7a2c1bb2228e414f5d36f34119b
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2023-10-15 08:51:46 +0200

    2.3.0-alpha.1 (13)

--- app/src/main/java/cz/jaro/rozvrh/nastaveni/NastaveniScreen.kt
+++ app/src/main/java/cz/jaro/rozvrh/nastaveni/NastaveniScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CircularProgressIndicator
