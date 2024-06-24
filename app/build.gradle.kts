51dadf794cc1143b85125c2d2c0276b6ffcc2075
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2024-06-24 17:02:16 +0200

    **2.5.0-alpha.1 (33)**

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "cz.jaro.rozvrh"
         minSdk = 26
         targetSdk = 34
-        versionCode = 32
-        versionName = "2.4.1-RC.1"
+        versionCode = 33
+        versionName = "2.5.0-alpha.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
