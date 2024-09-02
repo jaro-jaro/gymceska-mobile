c0f492d1353bbd93022be4e8f1838f2ba5525a4a
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2024-09-02 21:21:32 +0200

    2.4.2-RC.3 (36)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "cz.jaro.rozvrh"
         minSdk = 26
         targetSdk = 34
-        versionCode = 35
-        versionName = "2.4.2-RC.2"
+        versionCode = 36
+        versionName = "2.4.2-RC.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
