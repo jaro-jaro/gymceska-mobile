5d083689725b42fa4d5d2a1e3e7df1c2beb23387
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2024-09-02 20:00:25 +0200

    Refactor: Replace AlertDialog with custom dialog
    This commit replaces the usage of AlertDialog with a custom dialog implementation using the `cz.jaro.compose_dialog`
     library. This provides a more flexible and customizable dialog experience.
    
    The help dialog for rooms now utilizes the new dialog implementation, improving the overall user interface.

--- app/src/main/kotlin/cz/jaro/rozvrh/rozvrh/RozvrhScreen.kt
+++ app/src/main/kotlin/cz/jaro/rozvrh/rozvrh/RozvrhScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.material.icons.automirrored.filled.Help
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.PeopleAlt
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -25,9 +24,6 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -36,6 +32,8 @@ import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.spec.Direction
+import cz.jaro.compose_dialog.dialogState
+import cz.jaro.compose_dialog.show
 import cz.jaro.rozvrh.App.Companion.navigate
 import cz.jaro.rozvrh.ZdrojRozvrhu
 import org.koin.androidx.compose.koinViewModel
@@ -192,7 +190,6 @@ fun RozvrhContent(
                     .padding(horizontal = 4.dp),
             )
         }
-        var napoveda by remember { mutableStateOf(false) }
         Row(
             Modifier
                 .fillMaxWidth()
@@ -212,7 +209,17 @@ fun RozvrhContent(
                     IconButton(
                         onClick = {
                             hide()
-                            napoveda = true
+                            dialogState.show(
+                                confirmButton = { TextButton(::hide) { Text("OK") } },
+                                title = { Text("Nápověda k místnostem") },
+                                content = {
+                                    LazyColumn {
+                                        items(mistnosti.drop(1)) {
+                                            Text("${it.jmeno} - to je${it.napoveda}")
+                                        }
+                                    }
+                                },
+                            )
                         }
                     ) {
                         Icon(Icons.AutoMirrored.Filled.Help, null)
@@ -230,30 +237,6 @@ fun RozvrhContent(
                 label = vyucujici.first().jmeno,
             )
         }
-        if (napoveda) AlertDialog(
-            onDismissRequest = {
-                napoveda = false
-            },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        napoveda = false
-                    }
-                ) {
-                    Text("OK")
-                }
-            },
-            title = {
-                Text("Nápověda k místnostem")
-            },
-            text = {
-                LazyColumn {
-                    items(mistnosti.drop(1)) {
-                        Text("${it.jmeno} - to je${it.napoveda}")
-                    }
-                }
-            }
-        )
 
         if (tabulka == null) LinearProgressIndicator(Modifier.fillMaxWidth())
         else CompositionLocalProvider(LocalBunkaZoom provides zoom) {
