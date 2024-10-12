e9d2f565c1cfb1faf433ddf41a663e7dfabd4636
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2024-10-12 18:08:36 +0200

    Refactor: Redesign rozvrh screen UI
    This commit refactors the UI of the rozvrh screen by replacing the separate `Vybiratko` components
     with an `ExposedDropdownMenuBox`.
    It also changes the display of stalost (permanence) options to use a `SingleChoiceSegmentedButtonRow`.
    This improves the overall user experience and simplifies the layout.
    
    Additionally, the commit:
    - introduces a dialog for displaying room tooltips
    
    - rearranges the screen layout to accommodate these changes
    - extracts the logic for dropdown items into separate functions for better readability.

--- app/src/main/kotlin/cz/jaro/rozvrh/rozvrh/RozvrhScreen.kt
+++ app/src/main/kotlin/cz/jaro/rozvrh/rozvrh/RozvrhScreen.kt
@@ -1,10 +1,14 @@
 package cz.jaro.rozvrh.rozvrh
 
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,18 +18,33 @@ import androidx.compose.material.icons.automirrored.filled.Help
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.PeopleAlt
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.MenuDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ramcosta.composedestinations.annotation.Destination
@@ -101,7 +120,6 @@ fun Rozvrh(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RozvrhContent(
     tabulka: Tyden?,
@@ -146,97 +164,9 @@ fun RozvrhContent(
             .padding(paddingValues)
             .fillMaxSize()
     ) {
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .padding(vertical = 4.dp)
-                .padding(horizontal = 8.dp),
-            verticalAlignment = Alignment.Bottom
-        ) {
-            Vybiratko(
-                value = if (vjec is Vjec.TridaVjec) vjec else null,
-                seznam = tridy.drop(1),
-                onClick = { i, _ -> vybratRozvrh(tridy[i + 1]) },
-                Modifier
-                    .weight(1F)
-                    .padding(horizontal = 4.dp),
-                label = tridy.first().nazev,
-                trailingIcon = { hide ->
-                    if (zobrazitMujRozvrh) IconButton(
-                        onClick = {
-                            hide()
-                            zmenitMujRozvrh()
-                        }
-                    ) {
-                        Icon(if (mujRozvrh) Icons.Default.PeopleAlt else Icons.Default.Person, null)
-                    }
-                    else IconButton(
-                        onClick = {
-                            hide()
-                            vybratRozvrh(Vjec.TridaVjec("HOME"))
-                        }
-                    ) {
-                        Icon(Icons.Default.Home, null)
-                    }
-                },
-            )
+        Vybiratko(vjec, zobrazitMujRozvrh, zmenitMujRozvrh, mujRozvrh, vybratRozvrh, tridy, mistnosti, vyucujici)
 
-            Vybiratko(
-                value = stalost,
-                seznam = Stalost.dnesniEntries(),
-                onClick = { _, stalost -> zmenitStalost(stalost) },
-                Modifier
-                    .weight(1F)
-                    .padding(horizontal = 4.dp),
-            )
-        }
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .padding(vertical = 4.dp)
-                .padding(horizontal = 8.dp),
-            verticalAlignment = Alignment.Bottom,
-        ) {
-            Vybiratko(
-                value = if (vjec is Vjec.MistnostVjec) vjec else null,
-                seznam = mistnosti.drop(1),
-                onClick = { i, _ -> vybratRozvrh(mistnosti[i + 1]) },
-                Modifier
-                    .weight(1F)
-                    .padding(horizontal = 4.dp),
-                label = mistnosti.first().nazev,
-                trailingIcon = { hide ->
-                    IconButton(
-                        onClick = {
-                            hide()
-                            dialogState.show(
-                                confirmButton = { TextButton(::hide) { Text("OK") } },
-                                title = { Text("Nápověda k místnostem") },
-                                content = {
-                                    LazyColumn {
-                                        items(mistnosti.drop(1)) {
-                                            Text("${it.nazev} - to je${it.napoveda}")
-                                        }
-                                    }
-                                },
-                            )
-                        }
-                    ) {
-                        Icon(Icons.AutoMirrored.Filled.Help, null)
-                    }
-                },
-            )
-
-            Vybiratko(
-                value = if (vjec is Vjec.VyucujiciVjec) vjec else null,
-                seznam = vyucujici.drop(1),
-                onClick = { i, _ -> vybratRozvrh(vyucujici[i + 1]) },
-                Modifier
-                    .weight(1F)
-                    .padding(horizontal = 4.dp),
-                label = vyucujici.first().nazev,
-            )
-        }
+        PrepinatkoStalosti(stalost, zmenitStalost)
 
         if (tabulka == null) LinearProgressIndicator(Modifier.fillMaxWidth())
         else CompositionLocalProvider(LocalBunkaZoom provides zoom) {
@@ -259,3 +189,159 @@ fun RozvrhContent(
     }
 }
 
+@Composable
+private fun PrepinatkoStalosti(stalost: Stalost, zmenitStalost: (Stalost) -> Unit) = Row(
+    modifier = Modifier
+        .padding(horizontal = 8.dp)
+        .padding(top = 4.dp),
+) {
+    SingleChoiceSegmentedButtonRow(
+        modifier = Modifier
+            .weight(1F)
+            .height(IntrinsicSize.Max),
+    ) {
+        Stalost.entries.forEachIndexed { i, it ->
+            SegmentedButton(
+                selected = stalost == it,
+                onClick = { zmenitStalost(it) },
+                shape = SegmentedButtonDefaults.itemShape(i, Stalost.entries.count()),
+                Modifier.fillMaxHeight(),
+            ) {
+                Text(it.nazev)
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun Vybiratko(
+    vjec: Vjec,
+    zobrazitMujRozvrh: Boolean,
+    zmenitMujRozvrh: () -> Unit,
+    mujRozvrh: Boolean,
+    vybratRozvrh: (Vjec) -> Unit,
+    tridy: List<Vjec.TridaVjec>,
+    mistnosti: List<Vjec.MistnostVjec>,
+    vyucujici: List<Vjec.VyucujiciVjec>
+) {
+    var expanded by remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+        Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+            .padding(horizontal = 8.dp),
+    ) {
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .menuAnchor(MenuAnchorType.PrimaryNotEditable),
+            readOnly = true,
+            value = vjec.nazev,
+            onValueChange = {},
+            trailingIcon = {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    if (zobrazitMujRozvrh) IconButton(
+                        onClick = {
+                            zmenitMujRozvrh()
+                            expanded = false
+                            focusManager.clearFocus()
+                        }
+                    ) { Icon(if (mujRozvrh) Icons.Default.PeopleAlt else Icons.Default.Person, null) }
+                    else IconButton(
+                        onClick = {
+                            vybratRozvrh(Vjec.TridaVjec("HOME"))
+                            expanded = false
+                            focusManager.clearFocus()
+                        }
+                    ) { Icon(Icons.Default.Home, null) }
+
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded, Modifier.minimumInteractiveComponentSize())
+                }
+            },
+        )
+
+        val scrollState = rememberScrollState()
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {
+                expanded = false
+                focusManager.clearFocus()
+            },
+            scrollState = scrollState,
+        ) {
+            MenuVybiratka(tridy, mistnosti, vyucujici, vybratRozvrh) {
+                expanded = false
+                focusManager.clearFocus()
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun MenuVybiratka(
+    tridy: List<Vjec.TridaVjec>,
+    mistnosti: List<Vjec.MistnostVjec>,
+    vyucujici: List<Vjec.VyucujiciVjec>,
+    vybratRozvrh: (Vjec) -> Unit,
+    hide: () -> Unit,
+) {
+    val seznamy = listOf(tridy, mistnosti, vyucujici)
+    repeat(seznamy.maxOf { it.size }) { i ->
+        val vjeci = seznamy.map { it.getOrNull(i) }
+        Row {
+            vjeci.forEachIndexed { j, vjec ->
+                Box(
+                    Modifier.weight(listOf(5F, 7F, 12F)[j])
+                ) {
+                    if (vjec != null) DropdownMenuItem(
+                        text = {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    vjec.nazev
+                                )
+                                if (i == 0 && j == 1) NapovedaKMistostem(mistnosti)
+                            }
+                        },
+                        onClick = {
+                            vybratRozvrh(vjec)
+                            hide()
+                        },
+                        enabled = i != 0,
+                        colors = MenuDefaults.itemColors(
+                            disabledTextColor = MaterialTheme.colorScheme.primary
+                        ),
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun NapovedaKMistostem(mistnosti: List<Vjec.MistnostVjec>) = IconButton(
+    onClick = {
+        dialogState.show(
+            confirmButton = { TextButton(::hide) { Text("OK") } },
+            title = { Text("Nápověda k místnostem") },
+            content = {
+                LazyColumn {
+                    items(mistnosti.drop(1)) {
+                        Text("${it.nazev} - to je${it.napoveda}")
+                    }
+                }
+            },
+        )
+    }
+) {
+    Icon(Icons.AutoMirrored.Filled.Help, null)
+}
--- app/src/main/kotlin/cz/jaro/rozvrh/rozvrh/Vybiratko.kt
+++ app/src/main/kotlin/cz/jaro/rozvrh/rozvrh/Vybiratko.kt
@@ -1,6 +1,5 @@
 package cz.jaro.rozvrh.rozvrh
 
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
@@ -9,16 +8,14 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 
@@ -29,40 +26,12 @@ fun Vybiratko(
     seznam: List<Stalost>,
     onClick: (Int, Stalost) -> Unit,
     modifier: Modifier = Modifier,
-    trailingIcon: (@Composable (hide: () -> Unit) -> Unit)? = null,
-    colors: TextFieldColors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(),
 ) = Vybiratko(
     value = value.nazev,
     seznam = seznam.map { it.nazev },
     onClick = { i, _ -> onClick(i, seznam[i]) },
     modifier = modifier,
-    trailingIcon = trailingIcon,
     zaskrtavatko = { false },
-    colors = colors,
-)
-
-@ExperimentalMaterial3Api
-@Composable
-fun Vybiratko(
-    value: Vjec?,
-    seznam: List<Vjec>,
-    onClick: (Int, Vjec) -> Unit,
-    modifier: Modifier = Modifier,
-    label: String = "",
-    trailingIcon: (@Composable (hide: () -> Unit) -> Unit)? = null,
-    colors: TextFieldColors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(
-        unfocusedTextColor = /*if (value == null) ExposedDropdownMenuDefaults.outlinedTextFieldColors().unfocusedPlaceholderColor else*/ MaterialTheme.colorScheme.primary,
-        focusedTextColor = /*if (value == null) ExposedDropdownMenuDefaults.outlinedTextFieldColors().focusedPlaceholderColor else*/ MaterialTheme.colorScheme.primary,
-    ),
-) = Vybiratko(
-    value = value?.nazev ?: "",
-    seznam = seznam.map { it.nazev },
-    onClick = { i, _ -> onClick(i, seznam[i]) },
-    modifier = modifier,
-    label = label,
-    trailingIcon = trailingIcon,
-    zaskrtavatko = { false },
-    colors = colors,
 )
 
 @Composable
@@ -74,17 +43,13 @@ fun Vybiratko(
     modifier: Modifier = Modifier,
     label: String = "",
     zaskrtavatko: (String) -> Boolean = { it == seznam[index] },
-    trailingIcon: (@Composable (hide: () -> Unit) -> Unit)? = null,
-    colors: TextFieldColors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(),
 ) = Vybiratko(
     value = seznam[index],
     seznam = seznam,
     onClick = onClick,
     modifier = modifier,
     label = label,
-    trailingIcon = trailingIcon,
     zaskrtavatko = zaskrtavatko,
-    colors = colors,
 )
 
 @Composable
@@ -96,9 +61,7 @@ fun Vybiratko(
     modifier: Modifier = Modifier,
     label: String = "",
     zaskrtavatko: (String) -> Boolean = { it == value },
-    trailingIcon: (@Composable (hide: () -> Unit) -> Unit)? = null,
     zavirat: Boolean = true,
-    colors: TextFieldColors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(),
 ) {
     var expanded by remember { mutableStateOf(false) }
     val focusManager = LocalFocusManager.current
@@ -110,23 +73,15 @@ fun Vybiratko(
         OutlinedTextField(
             modifier = Modifier
                 .fillMaxWidth()
-                .menuAnchor(),
+                .menuAnchor(MenuAnchorType.PrimaryNotEditable),
             readOnly = true,
             value = value,
             onValueChange = {},
             label = { Text(label) },
             trailingIcon = {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    trailingIcon?.invoke {
-                        expanded = false
-                        focusManager.clearFocus()
-                    }
-                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
-                }
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
             },
-            colors = colors,
+            colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(),
         )
         ExposedDropdownMenu(
             expanded = expanded,
